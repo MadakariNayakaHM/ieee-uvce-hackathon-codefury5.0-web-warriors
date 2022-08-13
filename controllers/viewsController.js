8b1b6309b4f89aa753280065e539733e2ee73e00
MadakariNayakaHM <mahm20cs@cmrit.ac.in>
2022-08-13 23:55:38 +0530

    added dashboard feature and displaying the funded startups

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const express=require('express');
-const startUp=require('./../models/startupModel')
+const startUp=require('./../models/startupModel');
+const user=require('./../models/userModel');
 
 exports.createStartuppug=async (req,res,next)=>
 {
@@ -25,4 +26,20 @@ exports.displayStartup=async (req,res,next)=>
 exports.updateStartup=async (req,res,next)=>
 {
   res.status(200).render('innerFile');
+}
+exports.viewDashboard=async (req,res,next)=>{
+
+  const User= await user.countDocuments();
+  const startUpInit=await user.countDocuments({roles:"startupInit"})
+  const admin=await user.countDocuments({roles:"admin"})
+  const enterpreneur=await user.countDocuments({roles:"enterpreneur"})
+  const company=await user.countDocuments({roles:"company"})
+  const startups= await startUp.countDocuments();
+  const selected= await startUp.countDocuments({status:1});
+  res.status(200).render('dashboard' ,{startUpInit,enterpreneur,company,startups,selected,User,admin})
+}
+
+exports.viewSelected=async (req,res,next)=>
+{ const startup= await startUp.find();
+  res.status(200).render('selected',{startup});
 }
--- routers/viewsRoute.js
+++ routers/viewsRoute.js
@@ -6,5 +6,7 @@ Router.route('/login').get(viewsController.login);
 Router.route('/signup').get(viewsController.signup);
 Router.route('/display').get(viewsController.displayStartup);
 Router.route('/updateStartup').get(viewsController.updateStartup);
+Router.route('/viewDash').get(viewsController.viewDashboard);
+Router.route('/viewSelected').get(viewsController.viewSelected);
 
 module.exports=Router;
